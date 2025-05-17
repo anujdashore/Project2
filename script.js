d176264f1cfd7401734fbc8ca3f0b95dedfddd07
Anuj Dashore <anujdashore30@gmail.com>
2025-05-17 18:00:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ function initDatabase() {
               date: "2023-05-07",
               description: "Hydro Flask water bottle, 32oz, color mint green with some stickers.",
               contact: "gym.staff@example.com",
-              image: "bottel.jpg",
+              image: "waterBottel.jpg",
               reportedBy: "user9"
           },
           {
